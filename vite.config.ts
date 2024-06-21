89ced7f98d3905808108c499d0eba6c4d9737de5
Krzysztof Wilczyński <kwilczyn89@icloud.com>
2024-06-21 14:10:17 +0200

    Add example GithugActions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import packageJson from './package.json';
 
 export default defineConfig({
+  base: '/',
+  build: {
+    minify: true,
+    sourcemap: false,
+  },
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
   },
